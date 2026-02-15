7f157e39714a661f68e4b5affa4a806149e1a1ed
Keys <keys14052002@gmail.com>
2026-02-15 22:07:46 +0000

    project(rpg-game): define charcter class

---
+++ ruby_exercises/rpg_game/character.rb
@@ -0,0 +1,28 @@
+class Character
+  def initialize(name, health, mana, attack_power, status_effects)
+    @name = name
+    @health = health
+    @max_health = 100
+    @mana = mana
+    @attack_power = attack_power
+    @status_effects = status_effects
+  end
+
+  public
+  def attack(target)
+    puts "#{@name} just attacked #{target}"
+    target.take_damage(amount)
+  end
+
+  def take_damage(amount)
+    puts "#{@name} just took a damage of #{amount}"
+  end
+
+  def alive?
+    health > 0 ? True : False
+  end
+  
+  def apply_status(effect)
+    
+  end
+end
